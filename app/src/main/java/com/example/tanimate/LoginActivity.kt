fcbcdd6ec5f18b4a39537ab9fab8edf3b2d477ac
Khalid <mahfuz.hidayah@gmail.com>
2023-05-31 02:16:23 +0700

    Perubahan untuk fragment onBoarding untuk activity login adalah tujuan setelah button "Let's Start"

--- app/src/main/java/com/example/tanimate/LoginActivity.kt
+++ app/src/main/java/com/example/tanimate/LoginActivity.kt
@@ -3,9 +3,9 @@ package com.example.tanimate
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SplashScreenActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        setContentView(R.layout.activity_login)
     }
 }
