a58dc46aec290d9c396403aac4ec3b81551c9699
Khalid <mahfuz.hidayah@gmail.com>
2023-06-16 15:23:06 +0700

    Tambahan Profile Melengkapi

--- app/src/main/java/com/example/tanimate/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/example/tanimate/viewmodel/ProfileViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.tanimate.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.tanimate.data.local.UserSession
+import com.example.tanimate.data.network.ApiConfig
 import kotlinx.coroutines.launch
 
 class ProfileViewModel(private val pref: UserSession): ViewModel() {
+
 
     fun logout(){
         viewModelScope.launch {
